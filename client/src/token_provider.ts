af80ee664afc27f992f95ca5aabcf02cfd06649f
OttoOctavius <OttoOctavius@users.noreply.github.com>
2023-03-18 15:10:07 -0300

    contexto para tokens y refactor con los keywords

--- client/src/token_provider.ts
+++ client/src/token_provider.ts
@@ -1,4 +1,4 @@
-import { plotter, NodePlotter, keywords } from './highlighterDef'
+import { plotter, NodePlotter, keywords, tokenTypeObj } from './highlighterDef'
 import { Node, Singleton } from 'wollok-ts'
 
 //Nota: no todos los node's tienen .start (dando undefined), pueden provocar excepciones.
@@ -13,30 +13,59 @@ function extraerLineaColumna(node: Node, documentoStr: string[]) {
   }
 }
 
-function procesar(node: Node, documentoStr: string[]) {
+function procesar(node: Node, documentoStr: string[], context: NodeContext[]) {
+  const generar_plotter = node => {
+    const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
+    const col = columna + subStr.indexOf(node.name)
+    return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+  }
+  const keyword_plotter = (node, mensaje) => {
+    const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
+    const col = columna + subStr.indexOf(mensaje)
+    return plotter({ ln: linea, col: col, len: mensaje.length }, 'Keyword')
+  }
+  const save_reference = node => { return { name: node.name, type: node.kind }}
+  const drop_reference = node => { return { result: node, references: undefined }}
+  const null_case = { result: undefined, references: undefined }
+
+  if(node.kind === 'New'){ //por alguna razon no hace match
+    return drop_reference(keyword_plotter(node, keywords[node.kind]))
+  }
+
   return node.match({
     Class: node => {
-      const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
-      const col = columna + subStr.indexOf(node.name)
-      return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+      const acum = []
+      acum.push(keyword_plotter(node, 'class'))
+      node.supertypes.length>0 && acum.push(keyword_plotter(node, 'inherits'))
+      acum.push(generar_plotter(node))
+      return { result: acum, references: save_reference(node) }
     },
     Singleton: node => {
-      if(node.sourceMap == undefined) return undefined
-      const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
-      const col = columna + subStr.indexOf(node.name)
-      return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+      if(node.sourceMap == undefined) return null_case
+      const acum = []
+      node.members.reduce((prev, curr) => !curr.name.startsWith('<') && prev, true)
+        && acum.push(keyword_plotter(node, keywords[node.kind]))
+      acum.push(generar_plotter(node))
+      return { result: acum, references: save_reference(node) }
     },
     Field: node => {
-      const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
-      const col = columna + subStr.indexOf(node.name)
-      return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+      //if(node.c == '<toString>') return null_case
+      return {
+        result: [
+          generar_plotter(node),
+          keyword_plotter(node, keywords[node.kind]),
+        ],
+        references: save_reference(node),
+      }
     },
     Variable: node => {
-      const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
-      const col = columna + subStr
-        //.substring(node.sourceMap.start.column-1)
-        .indexOf(node.name)
-      return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+      return {
+        result: [
+          generar_plotter(node),
+          keyword_plotter(node, node.isConstant? 'const':'var'),
+        ],
+        references: save_reference(node),
+      }
     },
     Reference: node => {
       //node.variable
@@ -44,41 +73,77 @@ function procesar(node: Node, documentoStr: string[]) {
       //TODO: Si previamente hay un campo del mismo nombre no se toma
       //TODO: los parametros o propiedades se toman como nuevas referencias
       if(node.name == 'wollok.lang.Closure')
-        return undefined
-      const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
-      const col = columna + subStr
-        //.substring(columna)
-        .indexOf(node.name)
-      return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+        return null_case
+
+      const referencia  = context.find(x => x.name==node.name)
+      const pl = generar_plotter(node)
+      if(referencia){
+        pl.tokenType = tokenTypeObj[referencia.type]
+      }
+      return { result: pl, references: undefined } //no agrego informacion
     },
     Assignment: node => {
       //node.variable
       //node.value
       const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
       const col = columna + subStr.indexOf(node.variable.name)
-      return plotter({ ln: linea, col: col, len: node.variable.name.length }, node.kind)
+      return {
+        result: [
+          plotter({ ln: linea, col: col, len: node.variable.name.length }, node.kind),
+          keyword_plotter(node, keywords[node.kind]),
+        ], references: undefined,
+      }
     },
     Parameter: node => {
       const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
       const col = columna + subStr.indexOf(node.name)
-      return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+      return {
+        result: plotter({ ln: linea, col: col, len: node.name.length }, node.kind),
+        references: undefined,
+      }
     },
     Method: node => {
       if(node.name == '<apply>'){ //es un singleton closure
-        return undefined
+        return null_case
       }
+
       const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
       const col = columna + subStr.indexOf(node.name)
-      return plotter({ ln: linea, col: col, len: node.name.length }, node.kind)
+
+      return {
+        result: [
+          plotter({ ln: linea, col: col, len: node.name.length }, node.kind),
+          keyword_plotter(node, keywords[node.kind]),
+        ], references: undefined,
+      }
     },
     Send: node => {
-      if(keywords.Send.includes(node.message)) return undefined
-      const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
+      const curretKeyboard = keywords[node.kind]
+      const { linea, columna,  subStr } = extraerLineaColumna(node, documentoStr)
+      if(curretKeyboard && curretKeyboard.includes(node.message)){
+        if(node.message == 'negate'){//es la forma alternativa del simbolo '!'
+          const idx_negate = subStr.indexOf('!')
+          const col_offset: number= idx_negate == -1? subStr.indexOf('not'): idx_negate
+          const plotKeyboard =  plotter({
+            ln: linea,
+            col: columna + col_offset,
+            len: idx_negate == -1? 3: 1,
+          }, node.kind)
+          return drop_reference(plotKeyboard)
+        }
+        const col = columna + subStr.indexOf(node.message)
+        const plotKeyboard = plotter({ ln: linea, col: col, len: node.message.length }, node.kind)
+        return drop_reference(plotKeyboard)
+      }
+      //if(keywords.Send.includes(node.message)) return null_case
       const col = columna + subStr.indexOf(node.message)
-      return plotter({ ln: linea, col: col, len: node.message.length }, 'Method')//node.kind)
+      return {
+        result: plotter({ ln: linea, col: col, len: node.message.length }, 'Method'), //node.kind)
+        references: undefined,
+      }
     },
-    Return: _ => {
-      return undefined
+    Return: node => {
+      return drop_reference(keyword_plotter(node, keywords[node.kind]))
     },
     Literal: node => {
       const tipo = typeof node.value
@@ -90,116 +155,94 @@ function procesar(node: Node, documentoStr: string[]) {
             nodo
           })*/
         }
-        return undefined//plotter({ ln: linea, col: col, len: len }, 'Singleton')
+        return null_case//plotter({ ln: linea, col: col, len: len }, 'Singleton')
       }
+
       const { linea, columna, subStr } = extraerLineaColumna(node, documentoStr)
       switch (tipo) {
         case 'number':
         case 'bigint':
           const valor_numerico = node.value.toString()
-          return plotter({
+          return drop_reference(plotter({
             ln: linea,
             col: columna + subStr.indexOf(valor_numerico),
             len: valor_numerico.length,
-          }, 'Literal_number')
+          }, 'Literal_number'))
         case 'boolean':
           const valor_booleano = node.value.toString()
-          return plotter({
+          return drop_reference(plotter({
             ln: linea,
             col: columna + subStr.indexOf(valor_booleano),
             len: valor_booleano.length,
-          }, 'Literal_bool')
+          }, 'Literal_bool'))
         case 'string':
           const valor_string = node.value.toString()
-          return plotter({
+          return drop_reference(plotter({
             ln: linea,
             col: columna + subStr.indexOf(valor_string) - 1,
             len: valor_string.length + 2,
-          }, 'Literal_string')
+          }, 'Literal_string'))
         default:
-          return undefined
+          return null_case
       }
     },
-    Package: _ => undefined,
-    Import:  _ => undefined,
-    Program: _ => undefined,
-    Body:    _ => undefined,
-    Entity:  _ => undefined,
-    Sentence:    _ => undefined,
-    Expression:  _ => undefined,
-    Catch: _ => undefined,
-    Test:  _ => undefined,
-    ParameterizedType: _ => undefined,
-    NamedArgument:    _ => undefined,
+    Package: _ => null_case,
+    Import:  _ => null_case,
+    Program: _ => null_case,
+    Body:    _ => null_case,
+    Entity:  _ => null_case,
+    Sentence:    _ => {
+      return null_case
+    },
+    Expression:  _ => null_case,
+    Catch: _ => null_case,
+    Test:  _ => null_case,
+    ParameterizedType: _ => {
+      //console.log(node)
+      return null_case
+    },
+    NamedArgument:    _ => null_case,
 
-    Mixin:  _ => undefined,
-    Describe: _ => undefined,
+    Mixin:  _ => null_case,
+    Describe: _ => null_case,
 
-    Environment:  _ => undefined,
-    Try: _ => undefined,
-    Throw:    _ => undefined,
+    Environment:  _ => null_case,
+    Try: _ => null_case,
+    Throw:    _ => null_case,
 
     If: node => {
-      console.log(node)
-      return undefined
+      return drop_reference(keyword_plotter(node, keywords[node.kind]))
     },
     New: _ => {
-      return undefined
+      //return drop_reference(keyword_plotter(node, keywords[node.kind]))
+      return null_case
     },
     Super: _ => {
-      return undefined
+      //console.log(node)
+      return null_case
     },
   })
 }
 
-
-function excepcionKeyword(node: Node, mapKeyword: string|string[]){
-  if(node.kind == 'Singleton'){
-    return node.members.reduce((prev, curr) => !curr.name.startsWith('<') && prev, true)
-  }
-  if(node.kind == 'Method'){
-    return node.name!='<apply>'
-  }
-  if(node.kind == 'Send'){
-    return mapKeyword.includes(node.message)
-  }
-  return true
+type NodeContext = {name: string, type: string}
+type ProcesamientoCodigo = {
+  result: NodePlotter[];
+  references: NodeContext | NodeContext[];
 }
 
 export function processCode(node: Node, documentoStr: string[]): NodePlotter[] {
   return node.reduce((acum, node: Node) =>
   {
-    let curretKeyboard = keywords[node.kind]
+    const proc_nodo = procesar(node, documentoStr, acum.references)
 
-    if (curretKeyboard !== undefined && excepcionKeyword(node, curretKeyboard)){
-      const { linea, columna,  subStr } = extraerLineaColumna(node, documentoStr)
-      let kindType = 'Keyword' //node.kind
-      //||node.kind == 'Send'){
-      if(node.kind == 'Variable'){
-        curretKeyboard = node.isConstant? 'const':'var'
-      }
-      if(node.kind == 'Send'){ // && curretKeyboard.includes(node.message)){
-        curretKeyboard = node.message
-        kindType = 'Send'
-        if(curretKeyboard == 'negate'){//es la forma alternativa del simbolo '!'
-          const idx_negate = subStr.indexOf('!')
-          const col_offset: number= idx_negate == -1? subStr.indexOf('not'): idx_negate
-          const plotKeyboard =  plotter({
-            ln: linea,
-            col: columna + col_offset,
-            len: idx_negate == -1? 3: 1,
-          }, kindType)
-          return acum.concat(procesar(node, documentoStr)).concat(plotKeyboard)
-        }
-      }
-
-      const col = columna + subStr.indexOf(curretKeyboard)
-      const plotKeyboard = plotter({ ln: linea, col: col, len: curretKeyboard.length }, kindType)
-      return acum.concat(procesar(node, documentoStr)).concat(plotKeyboard)
+    return {
+      result: proc_nodo.result? acum.result.concat(proc_nodo.result):acum.result,
+      references: acum.references.concat(proc_nodo.references || []),
     }
-    return acum.concat(procesar(node, documentoStr))
-  }, [])
+  }, { result:[], references: [] }).result
 }
+//return { result: [...acum.result, procesar(node, documentoStr), plotKeyboard], references: acum.references }
+//return { result: [...acum.result, procesar(node, documentoStr), plotKeyboard], references: acum.references}
 
 //TODO: al no poder procesar comentarios multilinea se transforma a comentarios comunes.
 function plotterMultiLinea(arr: any[]) {
